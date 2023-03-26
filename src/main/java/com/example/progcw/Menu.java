0a7dea62f3150cfc081a9604aea145bc4721f3de
nuharameez <127706172+nuharameez@users.noreply.github.com>
2023-03-26 15:42:18 +0530

    STF complete
    check if the format of saving in text file needs to be changed

--- src/main/java/com/example/progcw/Menu.java
+++ src/main/java/com/example/progcw/Menu.java
@@ -1,14 +1,22 @@
 package com.example.progcw;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static com.example.progcw.DriverList.allDrivers;
 
@@ -17,6 +25,7 @@ public class Menu {
     public Menu(){
 
     }
+
 
     @FXML
     protected void onAddButtonClick(ActionEvent actionEvent) throws Exception {
@@ -126,12 +135,62 @@ public class Menu {
         previousStage.close();
     }
 
+
+
+
+
+
+
+
     @FXML
     protected void onSaveButtonClick(ActionEvent actionEvent) throws Exception {
-        navigateSaveData(actionEvent);
+        //navigateSaveData(actionEvent);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Did you save or load data in to the system?");
+        alert.setContentText("If yes selected data will be overwritten");
+
+        ButtonType buttonTypeYes = new ButtonType("Yes");
+        ButtonType buttonTypeNo = new ButtonType("No");
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+        Optional<ButtonType> result = alert.showAndWait();
+        List<List<String>> data = new ArrayList<>();
+        if(result.get() == buttonTypeYes) {
+
+            for (int i = 0; i < DriverList.allDrivers.size(); i++) {
+                data.add(Arrays.asList(String.valueOf(DriverList.allDrivers.get(i))));
+                try {
+                    FileWriter writer = new FileWriter("SavedDriverDetails.txt"); //ask user if they called stf or rff in the program, if yes get rid of the true.
+                    for (List<String> line : data) {
+                        writer.write(String.join(",", line) + "\n");
+                    }
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+            }
+        }
+        else{
+            for (int i = 0; i < DriverList.allDrivers.size(); i++) {
+                data.add(Arrays.asList(String.valueOf(DriverList.allDrivers.get(i))));
+                try {
+                    FileWriter writer = new FileWriter("SavedDriverDetails", true); //ask user if they called stf or rff in the program, if yes get rid of the true.
+                    for (List<String> line : data) {
+                        writer.write(String.join(",", line) + "\n");
+                    }
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+            }
+        }
     }
 
-    public void navigateSaveData(ActionEvent actionEvent) throws Exception  {
+    /*public void navigateSaveData(ActionEvent actionEvent) throws Exception  {
         Stage newStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("saveData.fxml"));
         newStage.setScene(new Scene(root,600,400 ));
@@ -139,7 +198,7 @@ public class Menu {
 
         Stage previousStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         previousStage.close();
-    }
+    }*/
 
     @FXML
     protected void onLoadButtonClick(ActionEvent actionEvent) throws Exception {
